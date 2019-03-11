45f290b3f9e52744e487bca4f5dd5af264b1f967
Ralfs <creepporkstudios@gmail.com>
2019-03-11 13:49:44 +0200

    Added Travis CI

--- config/database.php
+++ config/database.php
@@ -85,6 +85,18 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'testing' => [
+            'driver' => 'mysql',
+            'host' => env('DB_TEST_HOST', 'localhost'),
+            'database' => env('DB_TEST_DATABASE', 'forge'),
+            'username' => env('DB_TEST_USERNAME', 'forge'),
+            'password' => env('DB_TEST_PASSWORD', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+        ],
+
     ],
 
     /*
