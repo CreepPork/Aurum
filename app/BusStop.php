e7ba88fd9f3995e9277ec4aa47306b52e14502ef
Ralfs <creepporkstudios@gmail.com>
2019-03-11 17:03:54 +0200

    Added resource relations

--- app/BusStop.php
+++ app/BusStop.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class BusStop extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'stop_code', 'common_code', 'position_X', 'position_Y', 'road_side', 'road', 'street', 'region', 'parish', 'village'
+    ];
+
+    public function roadSide()
+    {
+        $this->hasOne(RoadSide::class);
+    }
+
+    public function region()
+    {
+        $this->hasOne(Region::class);
+    }
+
+    public function parish()
+    {
+        $this->hasOne(Parish::class);
+    }
+
+    public function village()
+    {
+        $this->hasOne(Village::class);
+    }
 }
--- app/TrainStop.php
+++ app/TrainStop.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class TrainStop extends Model
 {
-    //
+    protected $fillable = ['title', 'position_X', 'position_Y', 'line'];
+
+    /**
+     * A train stop has only one line assigned to it.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function line()
+    {
+        return $this->hasOne(Line::class);
+    }
 }
