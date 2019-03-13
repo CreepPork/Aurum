409725aa705fe839ebb1cee2455a17b76195fa31
Ralfs <creepporkstudios@gmail.com>
2019-03-13 19:52:07 +0200

    Added fillable property to sub-resources

--- app/Parish.php
+++ app/Parish.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Parish extends Model
 {
-    //
+    protected $fillable = ['title'];
 }
--- app/RoadSide.php
+++ app/RoadSide.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class RoadSide extends Model
 {
-    //
+    protected $fillable = ['title'];
 }
