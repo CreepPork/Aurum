ddb5441902215031a5ad3f6b2020ced50e85db90
Ralfs <creepporkstudios@gmail.com>
2019-03-13 19:49:46 +0200

    Added relationship to Line

--- app/Line.php
+++ app/Line.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Line extends Model
 {
     protected $fillable = ['title'];
+
+    public function stops()
+    {
+        return $this->hasMany(TrainStop::class);
+    }
 }
